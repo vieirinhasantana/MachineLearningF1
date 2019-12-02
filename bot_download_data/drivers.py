d9cc0cd67f5e601998a4e0d363f88b6fd0fc6aa1
Santana <daniel.santana@cedrotech.com>
2019-12-02 13:28:15 -0300

    News Features

--- bot_download_data/drivers.py
+++ bot_download_data/drivers.py
@@ -1,5 +1,6 @@
+import json
+
 import requests
-from bs4 import BeautifulSoup
 from logzero import logger
 from datetime import datetime
 
@@ -9,10 +10,15 @@ class Drivers:
         self._seasons = past_seasons
         self._session = requests.Session()
 
+    @property
+    def __get_current_year(self) -> int:
+        now = datetime.now()
+        return now.year
+
     def __page_source(self, year: int):
         try:
             response = self._session.get(
-                url=f'https://www.formula1.com/en/results.html/{year}/drivers.html',
+                url=f'http://ergast.com/api/f1/{year}/drivers.json?callback=drivers',
                 timeout=60
             )
             if not response.ok:
@@ -24,45 +30,20 @@ class Drivers:
             logger.error(f'Occurred error in use request. ERROR ---> {e}')
 
     @staticmethod
-    def __format_name_driver(name: str) -> (str, str):
-        clear_name = name.strip()
-        name_split = clear_name.split("\n")
+    def __extract_content_from_page(data, year) -> list:
+        _new_data = data.replace('drivers(', '').replace(')', '')
+        drivers = json.loads(_new_data)
+        for row in drivers.get('MRData', {}).get('DriverTable', {}).get('Drivers', []):
+            row['season'] = year
 
-        name_driver = f"{name_split[0]} { name_split[1]}"
-        alias = name_split[2]
-
-        return name_driver, alias
-
-    @staticmethod
-    def __extract_content_from_page(page_source, year):
-        soup = BeautifulSoup(page_source, 'html.parser')
-        table = soup.find('table', {'class': 'resultsarchive-table'})
-        for row in table.find_all('tr')[1:]:
-            _td = row.find_all('td')
-            name_driver, alias = Drivers.__format_name_driver(name=_td[2].get_text())
-
-            _obj = {
-                'year': year,
-                'pos': _td[1].get_text(),
-                'driver': name_driver,
-                'alias': alias,
-                'nationality': _td[3].get_text(),
-                'car': _td[4].get_text().strip(),
-                'pts': _td[5].get_text()
-            }
-            print(_obj)
-            break
-
-    @property
-    def __get_current_year(self) -> int:
-        now = datetime.now()
-        return now.year
+        return drivers.get('MRData', {}).get('DriverTable', {}).get('Drivers', [])
 
     def drivers(self):
         for season in range(self._seasons):
             search_by_year = self.__get_current_year - season
-            _source_page = self.__page_source(year=search_by_year)
-            Drivers.__extract_content_from_page(page_source=_source_page, year=search_by_year)
+            _data = self.__page_source(year=search_by_year)
+            _drivers = Drivers.__extract_content_from_page(data=_data, year=search_by_year)
+            print(_drivers)
 
 
 Drivers(past_seasons=6).drivers()
